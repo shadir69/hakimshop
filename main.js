68f17a198af1dc7b23d9eba295e682133f475370
Hachadi Younes <38957167+shadir69@users.noreply.github.com>
2023-06-25 21:52:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var product_side_figure = document.querySelector("#side-figure");
+
+product_side_figure.addEventListener("mousemove", function(e) {
+  var i = this.offsetWidth,
+    s = 800 - i,
+    d = this.getBoundingClientRect(),
+    t = (e.pageX - d.left) * s / i,
+    y = (e.pageY - d.top) * s / i;
+
+  this.classList.add("zoom-active");
+  this.querySelector("img").style.top = "-" + y + "px";
+  this.querySelector("img").style.left = "-" + t + "px";
+});
+
+product_side_figure.addEventListener("mouseleave", function() {
+  this.classList.remove("zoom-active");
+  this.querySelector("img").style.top = "0";
+  this.querySelector("img").style.left = "0";
+});
+
+window.addEventListener("sticky-header", function() {
+  if (is_admin_bar) {
+    product_side_figure.classList.add("sticky-header", "admin");
+  } else {
+    product_side_figure.classList.add("sticky-header");
+  }
+});
+
+window.addEventListener("un-sticky-header", function() {
+  if (is_admin_bar) {
+    product_side_figure.classList.remove("sticky-header", "admin");
+  } else {
+    product_side_figure.classList.remove("sticky-header");
+  }
+});
+/*********************************** */
+var orderResume = document.getElementById("order-resume");
+var orderResumeDetails = document.getElementById("order-resume-details");
+
+orderResume.addEventListener("click", function() {
+  this.classList.toggle("active");
+  orderResumeDetails.classList.toggle("active");
+});
+/****************************** */
+
